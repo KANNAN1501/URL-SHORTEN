b51dbfaafc1ad74a7cf7d38491985aba0016c356
KANNAN1501 <kannan20020115@gmail.com>
2025-03-31 12:50:08 +0530

    update url_shorten.py

--- url_shorten.py
+++ url_shorten.py
@@ -91,4 +91,4 @@ def redirect_url(short_url):
     return "URL Not Found", 404  # Handle non-existing short URLs
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
